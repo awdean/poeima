43f6533d57f97be747b820bcab30c93beba03348
Alexander Dean <alexanderdeancodes+github@gmail.com>
2015-08-18 20:55:09 -0400

    Added support for parsing terminal strings with petitparser.

---
+++ src/main/java/com/awdean/grammar/clipboard/element/Terminals.java
@@ -0,0 +1,67 @@
+package com.awdean.grammar.clipboard.element;
+
+import static org.petitparser.parser.primitive.CharacterParser.digit;
+import static org.petitparser.parser.primitive.CharacterParser.of;
+import static org.petitparser.parser.primitive.StringParser.of;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.Function;
+
+import org.petitparser.parser.Parser;
+import org.petitparser.utils.Functions;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Range;
+
+public class Terminals {
+
+	public static final String SECTION_BREAK = "--------";
+
+	public static Parser tFloat() {
+		// tFloat = /\d+(\.\d+)?/ ;
+		Parser parser = digit().plus(); // /\d+/
+		parser = parser.seq(of('.').seq(digit().plus()).optional()); // /\d+(\.\d+)?/
+		parser = parser.flatten().trim();
+		return parser.map((String value) -> new BigDecimal(value));
+	}
+
+	public static Parser tNumber() {
+		// tNumber = /\d+/ ;
+		Parser parser = digit().plus();
+		parser = parser.flatten().trim();
+		return parser.map((String value) -> Integer.parseInt(value));
+	}
+
+	public static Parser tRange() {
+		// tRange = /\d+-\d+/ ;
+		Parser parser = digit().plus();
+		parser = parser.seq(of('-'), digit().plus());
+		parser = parser.trim();
+		parser = parser.map(Functions.withoutSeparators());
+		return parser.map(new Function<List<List<Integer>>, ContiguousSet<Integer>>() {
+			@Override
+			public ContiguousSet<Integer> apply(List<List<Integer>> input) {
+				int lhs = Integer.parseInt(Joiner.on("").join(input.get(0)));
+				int rhs = Integer.parseInt(Joiner.on("").join(input.get(1)));
+				Range<Integer> range = Range.closed(lhs, rhs);
+				return ContiguousSet.create(range, DiscreteDomain.integers());
+			}
+		});
+	}
+
+	public static Parser tString() {
+		// tString = !tSectionBreak /[^\n]+/ ;
+		Parser parser = tSectionBreak().not();
+		parser = parser.seq(of('\n').neg().plus());
+		return parser.flatten().map((String value) -> value.trim());
+	}
+
+	public static Parser tSectionBreak() {
+		// tSectionBreak = "--------" ;
+		return of(SECTION_BREAK).flatten().trim();
+	}
+
+}
