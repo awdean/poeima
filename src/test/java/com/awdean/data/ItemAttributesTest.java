78d859a84f6af88fcf09a3489290fc68f4325003
Alexander Dean <alexanderdeancodes+github@gmail.com>
2015-08-20 01:30:59 -0400

    Modified unit tests for ItemAttributes to fully test equals method.

--- src/test/java/com/awdean/data/ItemAttributesTest.java
+++ src/test/java/com/awdean/data/ItemAttributesTest.java
@@ -35,13 +35,144 @@ public class ItemAttributesTest {
 
     @Test
     public void testEquals() {
-        assertThat(DEFAULT, is(DEFAULT));
-        assertThat(DEFAULT, is(not(equalTo(null)))); // need equalTo for null value
-        assertThat(DEFAULT, is(not(COMBINED)));
+        ItemAttributes head = new ItemAttributes();
+        ItemAttributes tail = new ItemAttributes();
         
-        assertThat(COMBINED, is(COMBINED));
-        assertThat(COMBINED, is(not(equalTo(null)))); // need equalTo for null value
-        assertThat(COMBINED, is(not(DEFAULT)));
+        // handle reflexive comparison
+        assertThat(head, is(head));
+        // handle compare-to-null short-circuit
+        assertThat(head, is(not(equalTo(null))));
+        // handle class-based comparison
+        assertThat(head, is(not(new Object())));
+
+        // iterate over all fields of the object
+        head.rarity = COMBINED.rarity;
+        assertThat(head, is(not(tail)));
+        tail.rarity = head.rarity;
+        assertThat(head, is(tail));
+        
+        head.name = COMBINED.name;
+        assertThat(head, is(not(tail)));
+        tail.name = head.name;
+        assertThat(head, is(tail));
+        
+        head.base = COMBINED.base;
+        assertThat(head, is(not(tail)));
+        tail.base = head.base;
+        assertThat(head, is(tail));
+        
+        head.type = COMBINED.type;
+        assertThat(head, is(not(tail)));
+        tail.type = head.type;
+        assertThat(head, is(tail));
+        
+        head.quality = COMBINED.quality;
+        assertThat(head, is(not(tail)));
+        tail.quality = head.quality;
+        assertThat(head, is(tail));
+        
+        head.armour = COMBINED.armour;
+        assertThat(head, is(not(tail)));
+        tail.armour = head.armour;
+        assertThat(head, is(tail));
+        
+        head.evasionRating = COMBINED.evasionRating;
+        assertThat(head, is(not(tail)));
+        tail.evasionRating = head.evasionRating;
+        assertThat(head, is(tail));
+        
+        head.energyShield = COMBINED.energyShield;
+        assertThat(head, is(not(tail)));
+        tail.energyShield = head.energyShield;
+        assertThat(head, is(tail));
+        
+        head.block = COMBINED.block;
+        assertThat(head, is(not(tail)));
+        tail.block = head.block;
+        assertThat(head, is(tail));
+        
+        head.physical = COMBINED.physical;
+        assertThat(head, is(not(tail)));
+        tail.physical = head.physical;
+        assertThat(head, is(tail));
+        
+        head.elemental = COMBINED.elemental;
+        assertThat(head, is(not(tail)));
+        tail.elemental = head.elemental;
+        assertThat(head, is(tail));
+        
+        head.chaos = COMBINED.chaos;
+        assertThat(head, is(not(tail)));
+        tail.chaos = head.chaos;
+        assertThat(head, is(tail));
+        
+        head.critical = COMBINED.critical;
+        assertThat(head, is(not(tail)));
+        tail.critical = head.critical;
+        assertThat(head, is(tail));
+        
+        head.attacks = COMBINED.attacks;
+        assertThat(head, is(not(tail)));
+        tail.attacks = head.attacks;
+        assertThat(head, is(tail));
+        
+        head.levelRequired = COMBINED.levelRequired;
+        assertThat(head, is(not(tail)));
+        tail.levelRequired = head.levelRequired;
+        assertThat(head, is(tail));
+        
+        head.strengthRequired = COMBINED.strengthRequired;
+        assertThat(head, is(not(tail)));
+        tail.strengthRequired = head.strengthRequired;
+        assertThat(head, is(tail));
+        
+        head.dexterityRequired = COMBINED.dexterityRequired;
+        assertThat(head, is(not(tail)));
+        tail.dexterityRequired = head.dexterityRequired;
+        assertThat(head, is(tail));
+        
+        head.intelligenceRequired = COMBINED.intelligenceRequired;
+        assertThat(head, is(not(tail)));
+        tail.intelligenceRequired = head.intelligenceRequired;
+        assertThat(head, is(tail));
+        
+        head.sockets = COMBINED.sockets;
+        assertThat(head, is(not(tail)));
+        tail.sockets = head.sockets;
+        assertThat(head, is(tail));
+        
+        head.level = COMBINED.level;
+        assertThat(head, is(not(tail)));
+        tail.level = head.level;
+        assertThat(head, is(tail));
+        
+        head.implicit = COMBINED.implicit;
+        assertThat(head, is(not(tail)));
+        tail.implicit = head.implicit;
+        assertThat(head, is(tail));
+        
+        head.explicits = COMBINED.explicits;
+        assertThat(head, is(not(tail)));
+        tail.explicits = head.explicits;
+        assertThat(head, is(tail));
+        
+        head.corrupted = COMBINED.corrupted;
+        assertThat(head, is(not(tail)));
+        tail.corrupted = head.corrupted;
+        assertThat(head, is(tail));
+        
+        head.mirrored = COMBINED.mirrored;
+        assertThat(head, is(not(tail)));
+        tail.mirrored = head.mirrored;
+        assertThat(head, is(tail));
+        
+        head.unidentified = COMBINED.unidentified;
+        assertThat(head, is(not(tail)));
+        tail.unidentified = head.unidentified;
+        assertThat(head, is(tail));
+        
+        // ensure that we've touched all the object's fields
+        assertThat(head, is(COMBINED));
     }
     
     @Test
